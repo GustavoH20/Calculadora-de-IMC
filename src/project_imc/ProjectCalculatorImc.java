131d60df7c4a06a44536d9d9beb1dfe623d8a8ee
Gustavo <gugas5897@gmail.com>
2025-11-13 23:58:51 -0300

    first codes in project

---
+++ src/project_imc/ProjectCalculatorImc.java
@@ -0,0 +1,41 @@
+package project_imc;
+
+import javafx.application.Application;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+
+public class ProjectCalculatorImc extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        //Etique para os campos de entrada
+        Label labelWeight = new Label("Weight");
+        Label labelHeigth = new Label("Heigth");
+
+        //Campos de textos para entreda de dados
+        TextField fieldWeigth = new TextField();
+        fieldWeigth.setPromptText("Weigth in kg");
+
+        TextField fieldHeight = new TextField();
+        fieldHeight.setPromptText("Height in meters");
+
+        //Label para mostrar o resultado do IMC
+        Label labelResult = new Label();
+
+        //Botão para calcular o IMC
+        Button buttonCalculate = new Button("Calculate IMC");
+        buttonCalculate.setOnAction(e -> {
+
+            //String sendo transformada em double
+            double weight = Double.parseDouble(fieldWeigth.getText());
+            double height = Double.parseDouble(fieldHeight.getText());
+
+            double imc = weight / (height * height);
+            labelResult.setText(String.format("Your IMS is: %.2f", imc));
+        });
+    }
+}
